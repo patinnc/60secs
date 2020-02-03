4ad0febd853d0fd14c5fe3a8267c41b68158c802
Patrick Fay <pfay1@ext.uber.com>
2020-02-03 15:08:07 -0800

    Allow for combining multiple dir's *txt.tsv files
    Currently tsv_2_xlsx.py just reads all the *.txt.tsv files and
    *.png's in a dir and creates a spreadsheet.
    Say we are doing a scaling study of perf_stat data for requests
    per second (RPS) at 1500, 1600, 1700, 1800 rps.
    The script can easily create an xlsx file for each perf_stat
    output.
    But I want to compare 1500 vs 1600 vs 1700 vs 1800.
    Opening each spreadsheet is a pain.
    This commit allows us to pass a file containing the options to be
    processed and 1 spreadsheet is created from the each set of
    options.
    So I can create 1 sheet with the perf_stat data from 1500-1800.
    See the previous commit for an example script which does this.

--- src/tsv_2_xlsx.py
+++ src/tsv_2_xlsx.py
@@ -18,141 +18,187 @@ import getopt
 import sys
 import glob
 
-options, remainder = getopt.getopt(sys.argv[1:], 'i:o:v', ['images=', 
+output_filename = 'chart_line.xlsx'
+opened_wkbk = False
+closed_wkbk = False
+
+options_filename = ""
+
+options, remainder = getopt.getopt(sys.argv[1:], 'f:i:o:p:v', ['file=', 'images=',
                                                          'output=',
+                                                         'prefix=',
                                                          'verbose',
                                                          ])
-output_filename = 'chart_line.xlsx'
-verbose = False
-image_files=[]
-
-print 'OPTIONS   :', options
-
 for opt, arg in options:
-    if opt in ('-i', '--images'):
-        for x in glob.glob(arg):
-           image_files.append(x)
-    elif opt in ('-o', '--output'):
-        output_filename = arg
-    elif opt in ('-v', '--verbose'):
-        verbose = True
+    if opt in ('-f', '--file'):
+        options_filename = arg
 
-workbook = xlsxwriter.Workbook(output_filename)
-
-def is_number(s):
-    try:
-        float(s)
-        return True
-    except ValueError:
-        return False
-
-#for x in sys.argv[1:]:
-for x in remainder:
-   
-   data = []
-   with open(x) as tsv:
+opt_fl = []
+fl_options = []
+if options_filename != "":
+   opt_fl = []
+   with open(options_filename) as tsv:
        for line in csv.reader(tsv, dialect="excel-tab"):
-           data.append(line)
-   #print data
-   
-   chrts = 0
-   ch_arr = []
-   sheet_nm = "sheet1"
-   ch_type  = "line"
-   for i in range(len(data)):
-       for j in range(len(data[i])):
-           if j == 0 and data[i][j] == "title":
-              print("got title for x= %s\n" % (x))
-              chrts = chrts + 1
-              ch = []
-              ch.append(["title", i, j])
-              if len(data[i]) >= 4:
-                 sheet_nm = data[i][3]
-              if len(data[i]) >= 6:
-                 ch_type = data[i][5]
-           if j == 0 and data[i][j] == "hdrs":
-              print("got hdrs for x= %s\n" % (x))
-              ch.append(["hdrs", i])
-              ch_arr.append(ch)
-           print data[i][j],
-       print
-   
-   print("sheet_nm= %s\n" % (sheet_nm))
-   wrksh_nm = sheet_nm
-   worksheet = workbook.add_worksheet(wrksh_nm)
-   bold = workbook.add_format({'bold': 1})
+           opt_fl.append(line)
+if len(opt_fl) > 0:
+   fl_opt = 0
+   fl_options.append([sys.argv[0]])
+   for i in range(len(opt_fl)):
+       if len(opt_fl[i]) == 0:
+          fl_opt = fl_opt + 1
+          fl_options.append([sys.argv[0]])
+          continue
+       for j in range(len(opt_fl[i])):
+           fl_options[fl_opt].append(opt_fl[i][j])
+else:
+   fl_opt = 0
+   fl_options.append([])
+   for i in range(len(sys.argv)):
+       fl_options.append([sys.argv[i]])
 
-   for c in range(chrts):
-       print("got chrt[%d] for x= %s\n" % (c, x))
-       title_rw = ch_arr[c][0][1]
-       title_cl = ch_arr[c][0][2]
-       drw  = ch_arr[c][1][1]
-       hrow_beg = int(data[drw][1])
-       hcol_beg = int(data[drw][2])
-       hrow_end = int(data[drw][1])
-       hcol_end = int(data[drw][4])+1
-       drow_beg = int(data[drw][1])+1
-       dcol_beg = int(data[drw][2])
-       drow_end = int(data[drw][3])
-       if drow_end == -1:
-          for i in range(drow_beg, len(data)):
-              drow_end = i
-              if len(data[i]) < dcol_beg:
-                 break
-          data[drw][3] = str(drow_end)
-       dcol_end = int(data[drw][4])+1
-       for i in range(drow_end-drow_beg+1):
-           for h in range(hcol_end):
-               if (h >= 0):
-                   is_num = is_number(data[i+drow_beg][h])
-                   if is_num:
-                      data[i+drow_beg][h] = float(data[i+drow_beg][h])
+for fo in range(len(fl_options)):
+   #options, remainder = getopt.getopt(sys.argv[1:], 'i:o:p:v', ['images=',
+   options, remainder = getopt.getopt(fl_options[fo][1:], 'i:o:p:v', ['images=',
+                                                            'output=',
+                                                            'prefix=',
+                                                            'verbose',
+                                                            ])
+   verbose = False
+   image_files=[]
+   prefix = ""
    
-   for i in range(len(data)):
-       worksheet.write_row(i, 0, data[i])
+   print 'OPTIONS   :', options
    
-   for c in range(chrts):
-       title_rw = ch_arr[c][0][1]
-       title_cl = ch_arr[c][0][2]
-       title = data[title_rw][title_cl+1]
-       drw  = ch_arr[c][1][1]
-       hrow_beg = int(data[drw][1])
-       hcol_beg = int(data[drw][2])
-       hrow_end = int(data[drw][1])
-       hcol_end = int(data[drw][4])+1
-       drow_beg = int(data[drw][1])+1
-       dcol_beg = int(data[drw][2])
-       drow_end = int(data[drw][3])
-       dcol_end = int(data[drw][4])+1
-       headings = []
-       for h in range(hcol_end):
-           if (h >= hcol_beg):
-              headings.append(data[hrow_beg][h])
-       #worksheet.write_row(hrow_beg, hcol_beg, headings, bold)
-       print ("sheet= %s ch= %d hro= %d hc= %d hce= %d, dr= %d, dre= %d" % (sheet_nm, c, hrow_beg, hcol_beg, hcol_end, drow_beg, drow_end))
-       chart1 = workbook.add_chart({'type': ch_type})
-       # Configure the first series.
-       for h in range(hcol_end):
-           if (h >= hcol_beg):
-               #    'categories': '=Sheet1!$A$2:$A$7',
-               chart1.add_series({
-                   'name':       [wrksh_nm, hrow_beg, h],
-                   'values':     [wrksh_nm, drow_beg, h, drow_end, h],
-               })
-       chart1.set_title ({'name': title})
-       chart1.set_style(10)
-       worksheet.insert_chart(hrow_beg, hcol_end, chart1, {'x_offset': 25, 'y_offset': 10})
-
-if len(image_files) > 0:
-   rw = len(image_files)
-   cl = 0
-   worksheet = workbook.add_worksheet('images')
-   for x in image_files:
-       worksheet.insert_image(rw, cl, x)
-       rw = rw - 1
-       cl = cl + 1
-
-workbook.close()
+   for opt, arg in options:
+       if opt in ('-i', '--images'):
+           for x in glob.glob(arg):
+              image_files.append(x)
+       elif opt in ('-o', '--output'):
+           output_filename = arg
+       elif opt in ('-p', '--prefix'):
+           prefix = arg
+       elif opt in ('-v', '--verbose'):
+           verbose = True
+   
+   if opened_wkbk == False:
+       workbook = xlsxwriter.Workbook(output_filename)
+       opened_wkbk = True
+   
+   def is_number(s):
+       try:
+           float(s)
+           return True
+       except ValueError:
+           return False
+   
+   for x in remainder:
+      
+      data = []
+      with open(x) as tsv:
+          for line in csv.reader(tsv, dialect="excel-tab"):
+              data.append(line)
+      #print data
+      
+      chrts = 0
+      ch_arr = []
+      sheet_nm = "sheet1"
+      ch_type  = "line"
+      for i in range(len(data)):
+          for j in range(len(data[i])):
+              if j == 0 and data[i][j] == "title":
+                 print("got title for x= %s\n" % (x))
+                 chrts = chrts + 1
+                 ch = []
+                 ch.append(["title", i, j])
+                 if len(data[i]) >= 4:
+                    sheet_nm = data[i][3]
+                 if len(data[i]) >= 6:
+                    ch_type = data[i][5]
+              if j == 0 and data[i][j] == "hdrs":
+                 print("got hdrs for x= %s\n" % (x))
+                 ch.append(["hdrs", i])
+                 ch_arr.append(ch)
+              print data[i][j],
+          print
+      
+      print("sheet_nm= %s\n" % (sheet_nm))
+      wrksh_nm = sheet_nm
+      worksheet = workbook.add_worksheet(wrksh_nm)
+      bold = workbook.add_format({'bold': 1})
+   
+      for c in range(chrts):
+          print("got chrt[%d] for x= %s\n" % (c, x))
+          title_rw = ch_arr[c][0][1]
+          title_cl = ch_arr[c][0][2]
+          drw  = ch_arr[c][1][1]
+          hrow_beg = int(data[drw][1])
+          hcol_beg = int(data[drw][2])
+          hrow_end = int(data[drw][1])
+          hcol_end = int(data[drw][4])+1
+          drow_beg = int(data[drw][1])+1
+          dcol_beg = int(data[drw][2])
+          drow_end = int(data[drw][3])
+          if drow_end == -1:
+             for i in range(drow_beg, len(data)):
+                 drow_end = i
+                 if len(data[i]) < dcol_beg:
+                    break
+             data[drw][3] = str(drow_end)
+          dcol_end = int(data[drw][4])+1
+          for i in range(drow_end-drow_beg+1):
+              for h in range(hcol_end):
+                  if (h >= 0):
+                      is_num = is_number(data[i+drow_beg][h])
+                      if is_num:
+                         data[i+drow_beg][h] = float(data[i+drow_beg][h])
+      
+      for i in range(len(data)):
+          worksheet.write_row(i, 0, data[i])
+      
+      for c in range(chrts):
+          title_rw = ch_arr[c][0][1]
+          title_cl = ch_arr[c][0][2]
+          title = data[title_rw][title_cl+1]
+          drw  = ch_arr[c][1][1]
+          hrow_beg = int(data[drw][1])
+          hcol_beg = int(data[drw][2])
+          hrow_end = int(data[drw][1])
+          hcol_end = int(data[drw][4])+1
+          drow_beg = int(data[drw][1])+1
+          dcol_beg = int(data[drw][2])
+          drow_end = int(data[drw][3])
+          dcol_end = int(data[drw][4])+1
+          headings = []
+          for h in range(hcol_end):
+              if (h >= hcol_beg):
+                 headings.append(data[hrow_beg][h])
+          #worksheet.write_row(hrow_beg, hcol_beg, headings, bold)
+          print ("sheet= %s ch= %d hro= %d hc= %d hce= %d, dr= %d, dre= %d" % (sheet_nm, c, hrow_beg, hcol_beg, hcol_end, drow_beg, drow_end))
+          chart1 = workbook.add_chart({'type': ch_type})
+          # Configure the first series.
+          for h in range(hcol_end):
+              if (h >= hcol_beg):
+                  #    'categories': '=Sheet1!$A$2:$A$7',
+                  chart1.add_series({
+                      'name':       [wrksh_nm, hrow_beg, h],
+                      'values':     [wrksh_nm, drow_beg, h, drow_end, h],
+                  })
+          chart1.set_title ({'name': title})
+          chart1.set_style(10)
+          worksheet.insert_chart(hrow_beg, hcol_end, chart1, {'x_offset': 25, 'y_offset': 10})
+   
+   if len(image_files) > 0:
+      rw = len(image_files)
+      cl = 0
+      worksheet = workbook.add_worksheet(prefix+'images')
+      for x in image_files:
+          worksheet.insert_image(rw, cl, x)
+          rw = rw - 1
+          cl = cl + 1
+   
+if closed_wkbk == False:
+    workbook.close()
+    closed_wkbk = True
     
 sys.exit(0)
 
