44991c537dbac97448d9c7207c5e3b6f00403a51
Patrick Fay <pfay1@ext.uber.com>
2020-02-20 21:31:30 -0800

    Add a setup script (does apt-get install ...)

---
+++ src/setup.sh
@@ -0,0 +1,17 @@
+echo "Installing Required Global packages"
+apt-get -y install sudo
+#apt-get -y install cpupower
+apt-get -y install msr-tools
+#apt-get -y install linux-cpupower
+apt-get -y install hwloc
+apt-get -y install hwinfo
+apt-get -y install cpuid
+apt-get -y install lscpu
+apt-get -y install lsblk
+apt-get -y install cpuid
+apt-get -y install util-linux # for lscpu
+apt-get -y install numactl
+apt-get -y install dmidecode
+apt-get -y install sysstat nicstat
+
+ 
