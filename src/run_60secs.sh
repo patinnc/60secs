a4a2d795fa401e3340d4db335ca18befa53ac4f3
Patrick Fay <pfay1@ext.uber.com>
2020-02-24 10:49:32 -0800

    Add option for prefix output dir with a date/time string
    Currently run_60secs.sh creates an output dir with a date time
    string prefixed. Like 20-02-24_155959_60secs.
    This can be useful but it also complicates post-processing.
    This commit makes adding the prefix optional.
    Use the -P option to add the prefix.
    Also, there is a bit of code to find the gmat docker container iD.
    This container doesn't exist on all machines.
    If the container isn't found then don't pass it to 60secs with
    -C containerID

--- src/run_60secs.sh
+++ src/run_60secs.sh
@@ -6,14 +6,18 @@ SCR_DIR=`dirname $(readlink -e $0)`
 CFG_DIR=
 echo "args= $*"
 DURA=1m
+PREFIX_DIR=0
 
-while getopts "hc:d:p:" opt; do
+while getopts "hPc:d:p:" opt; do
   case ${opt} in
     c )
       CFG_DIR=$OPTARG
       ;;
     d )
       DURA=$OPTARG
+      ;;
+    P )
+      PREFIX_DIR=1
       ;;
     p )
       PROJ_DIR=$OPTARG
@@ -26,6 +30,7 @@ while getopts "hc:d:p:" opt; do
       echo "     If you specify a project dir the benchmark results are looked for"
       echo "     under /project_dir/"
       echo "   -d duration of run in secs. Append 'm' for minutes"
+      echo "   -P prefix output dir with timestamp YY-MM-DD_HHMMSS_"
       exit
       ;;
     : )
@@ -42,14 +47,24 @@ mkdir -p $PROJ_DIR
 PROJ_DIR=`realpath -e $PROJ_DIR`
 
 timestamp=$(date '+%y-%m-%d_%H%M%S')
-result="$PROJ_DIR/${timestamp}_60secs"
+extra_dir=60secs
+if [ "$PREFIX_DIR" == "1" ]; then
+  extra_dir=${timestamp}_${extra_dir}
+fi
+result="$PROJ_DIR/${extra_dir}"
 mkdir -p $result
 
 pushd $result
 
 CNTNR=`docker ps | grep gmat | awk '{print $1;exit}'`
 
+if [ "$CNTNR" != "" ]; then
 $SCR_DIR/60secs.sh -t all -x top,interrupts -b -w -c -d $DURA -i 1 -p $SCR_DIR/perf -C $CNTNR
+else
+$SCR_DIR/60secs.sh -t all -x top,interrupts -b -w -c -d $DURA -i 1 -p $SCR_DIR/perf
+fi
 
 popd
+
+exit
 
