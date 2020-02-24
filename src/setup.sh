2fa8f8b193fca0f8e8d0930ef1e0354751df6cc4
Patrick Fay <pfay1@ext.uber.com>
2020-02-24 11:03:47 -0800

    update setup script to have right package names
    Some of the packages are needed by run_sysinfo, some by 60secs.sh.
    Not all of the packages are available on all OS's.

--- src/setup.sh
+++ src/setup.sh
@@ -1,17 +1,23 @@
 echo "Installing Required Global packages"
 apt-get -y install sudo
-#apt-get -y install cpupower
 apt-get -y install msr-tools
-#apt-get -y install linux-cpupower
 apt-get -y install hwloc
 apt-get -y install hwinfo
 apt-get -y install cpuid
-apt-get -y install lscpu
 apt-get -y install lsblk
-apt-get -y install cpuid
-apt-get -y install util-linux # for lscpu
+apt-get -y install util-linux
 apt-get -y install numactl
+apt-get -y install sysstat
+apt-get -y install nicstat
 apt-get -y install dmidecode
-apt-get -y install sysstat nicstat
-
- 
+##apt-get -y install cpupower
+##apt-get -y install linux-cpupower
+#apt-get -y install hwloc
+#apt-get -y install hwinfo
+#apt-get -y install lscpu
+#apt-get -y install lsblk
+#apt-get -y install cpuid
+#apt-get -y install util-linux # for lscpu
+#apt-get -y install numactl
+#apt-get -y install dmidecode
+#apt-get -y install sysstat nicstat
