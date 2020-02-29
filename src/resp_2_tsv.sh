98f93e028e0a54ee6e7fe4c0ce88565efa44f723
Patrick Fay <pfay1@ext.uber.com>
2020-02-28 22:33:47 -0800

    allow for beg/end time from cmdline

--- src/resp_2_tsv.sh
+++ src/resp_2_tsv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SCR_DIR=`dirname $0`
-echo "arg1 is RPS.json or response.json, arg2 is string for header"
+echo "arg1 is RPS.json or response.json, arg2 is string for header, arg3 and arg4 are optional begin and end timestamp (usually from the json file itself"
 FILE=$1
 if [ "$FILE" == "" ]; then
   echo "1st arg is json filename"
@@ -12,14 +12,23 @@ if [ ! -e $FILE ]; then
   exit
 fi
 STR=$2
-PRF_FILE="sys_10_perf_stat.txt"
+FORCE_BEG=$3
+FORCE_END=$4
+PRF_FILE=(sys_*_perf_stat.txt)
 if [ ! -e $PRF_FILE ]; then
-  echo "sorry but $0 depends (currently) on the $PRF_FILE existing in the cur dir"
+  if [ "$FORCE_BEG" == "" ]; then
+  echo "sorry but $0 depends (currently) on the $PRF_FILE existing in the cur dir, or get the 1st timestamp from the json file and pass it as arg3 to this script"
   exit
+  fi
 fi
-BEG=`cat 60secs.log | awk '{n=split($0, arr);printf("%s\n", arr[n]);exit;}'`
-DURA=`tail -1 $PRF_FILE | awk '{n=split($0, arr, ";");printf("%s\n", arr[1]);exit;}'`
-END=`awk -v beg="$BEG" -v dura="$DURA" 'BEGIN{printf("%f\n", beg+dura);exit;}'`
+if [ "$FORCE_BEG" == "" -o "$FORCE_END" == "" ]; then
+  BEG=`cat 60secs.log | awk '{n=split($0, arr);printf("%s\n", arr[n]);exit;}'`
+  DURA=`tail -1 $PRF_FILE | awk '{n=split($0, arr, ";");printf("%s\n", arr[1]);exit;}'`
+  END=`awk -v beg="$BEG" -v dura="$DURA" 'BEGIN{printf("%f\n", beg+dura);exit;}'`
+else
+  BEG=$FORCE_BEG
+  END=$FORCE_END
+fi
 
 echo "beg= $BEG, DURA= $DURA, end= $END"
 python $SCR_DIR/json_2_tsv.py $FILE $BEG $END $STR
