d0164a503d710bc8d66b539823f91150ab4c87de
Patrick Fay <pfay1@ext.uber.com>
2020-04-15 11:34:54 -0400

    Change the call to json_2_tsv.py for getopts

--- src/resp_2_tsv.sh
+++ src/resp_2_tsv.sh
@@ -2,18 +2,62 @@
 
 SCR_DIR=`dirname $0`
 echo "arg1 is RPS.json or response.json, arg2 is string for header, arg3 and arg4 are optional begin and end timestamp (usually from the json file itself"
-FILE=$1
+
+FILE=
+BEG_IN=
+END_IN=
+SUM_FILE=
+TYP_IN=
+
+while getopts "hvf:b:e:s:" opt; do
+  case ${opt} in
+    f )
+      FILE=$OPTARG
+      ;;
+    b )
+      BEG_IN=$OPTARG
+      ;;
+    e )
+      END_IN=$OPTARG
+      ;;
+    s )
+      SUM_FILE=$OPTARG
+      ;;
+    t )
+      HDR=$OPTARG
+      ;;
+    v )
+      VERBOSE=$((VERBOSE+1))
+      ;;
+    h )
+      echo "$0 split data files into columns"
+      echo "Usage: $0 [-h] -f json_file -t header [ -b beg_timestamp -e end_timestamp ] -s summary_filename [-v]"
+      echo "   -v verbose mode"
+      exit
+      ;;
+    : )
+      echo "Invalid option: $OPTARG requires an argument" 1>&2
+      exit
+      ;;
+    \? )
+      echo "Invalid option: $OPTARG" 1>&2
+      exit
+      ;;
+  esac
+done
+shift $((OPTIND -1))
+
 if [ "$FILE" == "" ]; then
-  echo "1st arg is json filename"
+  echo "need -f json_filename"
   exit
 fi
 if [ ! -e $FILE ]; then
-  echo "1st arg is json filename and file $FILE not found"
+  echo "didn't find -f $FILE json file"
   exit
 fi
-STR=$2
-FORCE_BEG=$3
-FORCE_END=$4
+TYP=$HDR
+FORCE_BEG=$BEG_IN
+FORCE_END=$END_IN
 PRF_FILE=(sys_*_perf_stat.txt)
 if [ ! -e $PRF_FILE ]; then
   if [ "$FORCE_BEG" == "" ]; then
@@ -31,5 +75,17 @@ else
 fi
 
 echo "beg= $BEG, DURA= $DURA, end= $END"
-python $SCR_DIR/json_2_tsv.py $FILE $BEG $END $STR
+O_BEG=
+if [ "$BEG" != "" ]; then
+  O_BEG=" -b $BEG "
+fi
+O_END=
+if [ "$END" != "" ]; then
+  O_END=" -e $END "
+fi
+O_TYP=
+if [ "$TYP" != "" ]; then
+  O_TYP=" -t $TYP "
+fi
+python $SCR_DIR/json_2_tsv.py -f $FILE -s $SUM_FILE $O_BEG $O_END $O_TYP
 #python ../json_2_tsv.py response_time.json $BEG $END
