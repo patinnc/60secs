c97240c83773ea79cfa0b9a4271b38ac4a6267b8
Patrick Fay <pfay1@ext.uber.com>
2020-04-26 21:01:45 -0400

    Add script to collect power via ipmitool
    This script collects power for a server using the ipmitool.
    It sometimes call other ipmi based tools which handle user ids
    and passwords.
    I've tried various APIs. The max and average over an interval does
    not seem to work, at least not to me.
    So I get the instantaneous value and compute an average and max.

---
+++ src/collect_power.sh
@@ -0,0 +1,288 @@
+#!/bin/bash
+
+#     ww  ww  qct qct b19a b19a b20b b20b
+HLST="160 240 131 177 137  53   167   55"
+BLST="127 234 182 175 253 191   154  164"
+TLST="w   w   q   q   q   q     w    w"
+
+HARR=($(echo "$HLST" | tr ' ' '\n'))
+BARR=($(echo "$BLST" | tr ' ' '\n'))
+TARR=($(echo "$TLST" | tr ' ' '\n'))
+echo "HLST= $HLST"
+echo "BLST= $BLST"
+echo "TARR= ${TARR[@]}"
+HOST_IN=
+DCMI_USE=
+DELLOEM=
+
+while getopts "hvbcDwza:B:H:C:d:i:p:t:x:" opt; do
+  case ${opt} in
+    a )
+      ADD_IN=$OPTARG
+      ;;
+    b )
+      BKGRND=1
+      ;;
+    B )
+      BMC=$OPTARG
+      ;;
+    c )
+      CURL_AT_END=1
+      ;;
+    D )
+      DELLOEM=1
+      ;;
+    C )
+      CONTAINER=$OPTARG
+      ;;
+    w )
+      WAIT_AT_END=1
+      ;;
+    d )
+      WAIT_IN=$OPTARG
+      ;;
+    i )
+      INTERVAL=$OPTARG
+      ;;
+    H )
+      HOST_IN=$OPTARG
+      ;;
+    x )
+      EXCLUDE=$OPTARG
+      ;;
+    p )
+      if [ "$OPTARG" != "" -a ! -x $OPTARG ]; then
+        echo "didn't find perf binary. You entered \"-p $OPTARG\""
+        exit
+      fi
+      PERF_BIN=$OPTARG
+      ;;
+    v )
+      VERBOSE=$((VERBOSE+1))
+      ;;
+    z )
+      DCMI_USE=1
+      ;;
+    h )
+      echo "usage: $0 -t task_num|task_name[,taskname[...]] [ -b ] -d seconds_to_run -i sample_interval_in_secs [ -p full_path_of_perf_binary ]"
+      echo "task_num is 0 to $TLAST or -1 for all tasks"
+      echo "seconds_to_run is how long you want each task to monitor the system. Defaults is $WAIT seconds."
+      echo "data collected once a second till for $WAIT seconds."
+      echo "task_names are: ${TASKS[@]}"
+      echo "   -t task_num or task_name"
+      echo "      The task names: ${TASKS[@]}"
+      echo "      Enter '-t all' for all tasks except flamegraph and sched_switch (sched_switch which can write 100s of MBs of data per 10 sec interval... so it has more overhead)"
+      echo "      Valid task_num range is 0 to $TLAST"
+      echo "      Enter either the number or the task name in a comma separated list"
+      echo "      default is no task"
+      echo "   -a flamegraph and/or sched_switch"
+      echo "      if you do '-t all' flamegraph and sched_switch are not run"
+      echo "      Use this option to add flamegraph and or sched_switch"
+      echo "   -x task_num or task_name"
+      echo "      A list of tasks to be excluded. top and interrupts are not too useful to me"
+      echo "      Have to enter the task name, not number"
+      echo "   -b start the jobs in the backgroup not waiting for each job to finish"
+      echo "      The default is to not start the jobs the background (so start the 1st task, wait for it to finish, start the next task, etc)"
+      echo "   -w If you run background mode, this option waits for the duration after the background jobs are started."
+      echo "   -c use this option to run ${SCR_DIR}/do_curl.sh at the end"
+      echo "   -C container ID to pass to ${SCR_DIR}/do_curl.sh and/or flamegraph script"
+      echo "   -d time_in_seconds duration of each monitoring task in seconds"
+      echo "      You can append an 'm' to specify the duration in minutes (like '-d 10m' which sets the duration to 600 seconds)"
+      echo "      default is $WAIT seconds"
+      echo "   -D use delloem interface"
+      echo "   -i interval in seconds between data collection"
+      echo "      default is $INTRVL seconds"
+      echo "   -p full_path_to_perf_binary  the perf binary might not be in the path so use this option to specify it"
+      echo "      default is 'perf'"
+      echo "   -v verbose mode. display each file after creating it."
+      echo "   -z use dcmi power reading cmd"
+      exit
+      ;;
+    : )
+      echo "Invalid option: $OPTARG requires an argument" 1>&2
+      exit
+      ;;
+    \? )
+      echo "Invalid option: $OPTARG" 1>&2
+      exit
+      ;;
+  esac
+done
+shift $((OPTIND -1))
+
+GOT_QUIT=0
+# function called by trap
+catch_signal() {
+    printf "\rSIGINT caught      "
+    GOT_QUIT=1
+}
+
+trap 'catch_signal' SIGINT
+
+if [ "$HOST_IN" != "" ]; then
+  HOST=$HOST_IN
+fi
+
+if [ "$DELLOEM" == "1" ]; then
+     GOT=1
+     UBMC=""
+     TYP=DELLOEM
+else
+if [ "$HOST" != "" ];then
+  if [[ $HOST == *"."* ]]; then
+   echo 'got . in $HOST'
+   HOST=`echo $HOST| sed 's/.*\.//g'`
+   echo "HOST= $HOST"
+  else
+   echo 'not got .'
+  fi
+  GOT=0
+  for i in `seq 0 ${#HARR[@]}`; do
+    echo "i= $i"
+    if [ "${HARR[$i]}" == "$HOST" ]; then
+     GOT=1
+     UBMC=${BARR[$i]}
+     TYP=${TARR[$i]}
+     break
+    fi
+  done
+  if [ "$GOT" == "0" ]; then
+   echo "missed look of HOST= $HOST"
+   echo "here is the list ${HARR[@]}"
+   exit
+  fi
+fi
+if [ "$BMC" != "" ];then
+  if [[ $BMC == *"."* ]]; then
+   echo 'got . in $BMC'
+   BMC=`echo $BMC| sed 's/.*\.//g'`
+   echo "BMC= $BMC"
+  else
+   echo 'not got .'
+  fi
+  GOT=0
+  for i in `seq 0 ${#BARR[@]}`; do
+    if [ "${BARR[$i]}" == "$BMC" ]; then
+     GOT=1 
+     UBMC=${BARR[$i]}
+     HOST=${HARR[$i]}
+     TYP=${TARR[$i]}
+     break
+    fi
+  done
+  if [ "$GOT" == "0" ]; then
+   echo "missed look of BMC= $BMC"
+   echo "here is the list ${BARR[@]}"
+  fi
+fi
+fi
+echo "ubmc= $UBMC"
+
+if [ "$TYP" == "" ]; then
+  echo "missed TYP lookup for HOST $HOST and BMC $UBMC" > /dev/stderr
+  echo "add host and/or BMD to TLST. Bye" > /dev/stderr
+  exit
+fi
+
+
+if [ "$WAIT_IN" != "" ]; then
+  RESP=`echo $WAIT_IN | sed 's/m//i'`
+  if [ "$RESP" != "$WAIT_IN" ]; then
+    WAIT_IN=$((RESP*60))
+  fi
+  re='^[0-9]+$'
+  if ! [[ $WAIT_IN =~ $re ]] ; then
+     echo "error: duration -d $WAIT_IN is not a number" >&2; exit 1
+  fi
+  if [ $WAIT_IN -lt 1 ]; then
+    echo "you entered \"-d $WAIT_IN\" duration must be > 1"
+    exit
+  fi
+  WAIT=$WAIT_IN
+else
+  WAIT=60
+fi
+echo "using interval $WAIT seconds"
+
+if [ "$INTERVAL" != "" ]; then
+  re='^[0-9]+$'
+  if ! [[ $INTERVAL =~ $re ]] ; then
+     echo "error: interval -i $INTERVAL is not a number" >&2; exit 1
+  fi
+  if [ $INTERVAL -lt 1 ]; then
+    echo "you entered \"-i $INTERVAL\" duration must be > 0"
+    exit
+  fi
+  INTRVL=$INTERVAL
+else
+  INTRVL=1
+fi
+echo "using interval $INTRVL seconds"
+
+TSK=power
+DO_POWER=1
+if [ "$DELLOEM" == "1" ]; then
+  echo "ipmi delloem powermonitor clar cumulativepower and peakpower"
+  ipmi $HOST delloem powermonitor clear cumulativepower
+  ipmi $HOST delloem powermonitor clear peakpower
+  if [ "$DCMI_USE" != "1" ]; then
+    CMD="ipmi $HOST delloem powermonitor powerconsumptionhistory"
+  else
+    CMD="ipmi $HOST dcmi power reading"
+  fi
+else
+  if [ "$DCMI_USE" != "1" ]; then
+    CMD="./ipmi-${TYP} $UBMC sdr"
+  else
+    CMD="./ipmi-${TYP} $UBMC dcmi power reading"
+  fi
+fi
+FLNUM=16
+  if [[ $TSK == *"power"* ]]; then
+    if [ "$DO_POWER" == "1" ]; then
+    FL=$HOST.sys_${FLNUM}_power.txt
+    if [ -e $FL ]; then
+      rm $FL
+    fi
+    echo "$CMD"
+    j=0
+    BDT=`date +%s` 
+    EDT=$((BDT+$WAIT))
+    if [ "$BKGRND" == "1" ]; then
+      FL_PWR=$FL
+    else
+    for i in `seq 1 $WAIT`; do
+      #echo "i= $i of $WAIT"
+      DT=`date +%s.%N`
+      echo "==beg $j date $DT" >> $FL
+      if [ "$DCMI_USE" != "1" ]; then
+        ./ipmi-${TYP} $UBMC sdr >> $FL
+      else
+        ./ipmi-${TYP} $UBMC dcmi power reading >> $FL
+      fi
+      #$CMD >> $FL
+      #ipmitool sdr >> $FL
+      DT=`date +%s.%N`
+      echo "==end $j date $DT" >> $FL
+      CDT=`date +%s`
+      ELAP=$(($CDT-$BDT))
+      j=$((j+$INTRVL))
+      if [ $j -ge $WAIT ]; then
+        break
+      fi
+      if [ $CDT -ge $EDT ]; then
+        break
+      fi
+      printf "\rpower i= %d of %d, elap secs= %d curtm= %d, endtm= %d" $i $WAIT $ELAP $CDT  $EDT
+      sleep $INTRVL
+      if [ "$GOT_QUIT" == "1" ]; then
+         break
+      fi
+    done 
+    printf "\n"
+    fi
+    if [ "$DO_POWER" == "1" ]; then
+      echo "=======did power ========="
+    fi
+    fi
+  fi  
