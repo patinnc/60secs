808da9bf425bb92bd94346e7a381227b0c5366cc
Patrick Fay <pfay1@ext.uber.com>
2020-09-16 10:57:49 -0400

    Add more -h info and add -C container_grep_str option
    Currently the -h help info isn't complete. Fix that.
    Also, the container id might need to be passed to 60secs.sh.
    The new -C container_str is used to get a container ID by doing:
      docker ps |grep container_str | head -1  and get the ID.

--- run_60secs.sh
+++ run_60secs.sh
@@ -12,14 +12,18 @@ ADD_IN=
 INTRVL=1
 EXCL=
 EVT_IN=
+DO_CONTAINER=
 
-while getopts "hPa:c:d:E:i:p:t:W:x:" opt; do
+while getopts "hPa:c:C:d:E:i:p:t:W:x:" opt; do
   case ${opt} in
     a )
       ADD_IN=$OPTARG
       ;;
     c )
       CFG_DIR=$OPTARG
+      ;;
+    C )
+      DO_CONTAINER=$OPTARG
       ;;
     d )
       DURA=$OPTARG
@@ -53,8 +57,15 @@ while getopts "hPa:c:d:E:i:p:t:W:x:" opt; do
       echo "     by default the host results dir name $PROJ_DIR"
       echo "     If you specify a project dir the benchmark results are looked for"
       echo "     under /project_dir/"
+      echo "   -C Container_string  if you are doing a container flamegraph, add a string for 'docker ps | grep string' to get the container id."
+      echo "   -c config_dir"
+      echo "   -i interval for collection in seconds."
       echo "   -d duration of run in secs. Append 'm' for minutes"
       echo "   -P prefix output dir with timestamp YY-MM-DD_HHMMSS_"
+      echo "   -a add_in_tasks  some tasks like flamegraphs are not in the default list"
+      echo "   -t tasks_to_run  comma separated list of tasks to run. Overrides the default list."
+      echo "   -x tasks_not_to_run comma separated list of tasks (from default task list) to not run"
+      echo "   -E event event for flamegraphs (lock or itimer). default is itimer."
       echo "   -W watch_cmd"
       exit
       ;;
@@ -85,7 +96,11 @@ echo "using output dir= $result"
 
 pushd $result
 
-CNTNR=`docker ps | grep gmat | awk '{print $1;exit}'`
+hostname > hostname.txt
+
+if [ "$DO_CONTAINER" != "" ]; then
+  CNTNR=`docker ps | grep $DO_CONTAINER | awk '{print $1;exit}'`
+fi
 
 TASK=all
 if [ "$TASK_IN" != "" ]; then
@@ -115,8 +130,8 @@ if [ "$CNTNR" != "" ]; then
 echo $SCR_DIR/60secs.sh -t $TASK $OPT_EX -b -w -c -d $DURA -i $INTRVL -p $SCR_DIR/perf -C $CNTNR $OPT_EV -W "$WATCH_IN" $ADD
      $SCR_DIR/60secs.sh -t $TASK $OPT_EX -b -w -c -d $DURA -i $INTRVL -p $SCR_DIR/perf -C $CNTNR $OPT_EV -W "$WATCH_IN" $ADD
 else
-echo $SCR_DIR/60secs.sh -t $TASK $OPT_EX -b -w -c -d $DURA -i $INTRVL -p $SCR_DIR/perf $OPT_EV -W "$WATCH_IN" $ADD
-     $SCR_DIR/60secs.sh -t $TASK $OPT_EX -b -w -c -d $DURA -i $INTRVL -p $SCR_DIR/perf $OPT_EV -W "$WATCH_IN" $ADD
+echo $SCR_DIR/60secs.sh -t $TASK $OPT_EX -b -w -d $DURA -i $INTRVL -p $SCR_DIR/perf $OPT_EV -W "$WATCH_IN" $ADD
+     $SCR_DIR/60secs.sh -t $TASK $OPT_EX -b -w -d $DURA -i $INTRVL -p $SCR_DIR/perf $OPT_EV -W "$WATCH_IN" $ADD
 fi
 
 popd
