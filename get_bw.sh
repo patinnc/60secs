4bdffda4ce7a36e4975e9ca8ac537d96c08de30a
Patrick Fay <pfay1@ext.uber.com>
2021-06-25 01:54:15 -0400

    Simplify code, add ice lake events and metrics

--- get_bw.sh
+++ get_bw.sh
@@ -43,43 +43,60 @@ awk -v sockets="${LSCPU_INFO[3]}" -v vendor="${LSCPU_INFO[2]}" -v tsc_ghz="${LSC
    }
   BEGIN{
     i=0;
-    UNC = ++i; hdr[UNC] = "mem_bw";
-    L3m = ++i; hdr[L3m] = "L3misses";
-    L3a = ++i; hdr[L3a] = "L3access";
-    pwr = ++i; hdr[pwr] = "pkg_watts";
-    pfl = ++i; hdr[pfl] = "pf_lcl";
-    pfr = ++i; hdr[pfr] = "pf_rmt";
-    meml   = ++i;  hdr[meml]    = "demand_lcl";
-    memr   = ++i;  hdr[memr]    = "demand_rmt"
-    L3cyc  = ++i;  hdr[L3cyc]   = "L3miss_out"
-    cyc    = ++i;  hdr[cyc]     = "cycles"
-    mpf    = ++i;  hdr[mpf]     = "mperf"
-    uop_ret= ++i;  hdr[uop_ret] = "ret_uops_cycles";
-    irprf  = ++i;  hdr[irprf]   = "instr"
+    #UNC = ++i; hdr[UNC] = "mem_bw";
+    #L3m = ++i; hdr[L3m] = "L3misses";
+    #L3a = ++i; hdr[L3a] = "L3access";
+    #pwr = ++i; #hdr[pwr] = "pkg_watts";
+    #pfl = ++i; hdr[pfl] = "pf_lcl";
+    #pfr = ++i; hdr[pfr] = "pf_rmt";
+    #meml   = ++i;  hdr[meml]    = "demand_lcl";
+    #memr   = ++i;  hdr[memr]    = "demand_rmt"
+    #L3cyc  = ++i;  hdr[L3cyc]   = "L3miss_out"
+    #cyc    = ++i;  hdr[cyc]     = "cycles"
+    #mpf    = ++i;  #hdr[mpf]     = "mperf"
+    #uop_ret= ++i;  hdr[uop_ret] = "ret_uops_cycles";
+    #irprf  = ++i;  hdr[irprf]   = "instr"
     j =0;
-    lkup[++j] = "qpi_data_bandwidth_tx0";
-    lkup[++j] = "qpi_data_bandwidth_tx1";
-    lkup[++j] = "unc_cha_tor_inserts.ia_miss.0x40433";
-    lkup[++j] = "unc_cha_tor_occupancy.ia_miss.0x40433";
-    lkup[++j] = "unc_cha_clockticks";
-    lkup[++j] = "power/energy-pkg/";
-    lkup[++j] = "ref-cycles";
-    lkup[++j] = "offcore_requests.l3_miss_demand_data_rd";
-    lkup[++j] = "offcore_requests_outstanding.l3_miss_demand_data_rd";
-    lkup[++j] = "unc_cha_tor_inserts.ia.0x40433";
-    lkup[++j] = "upi_data_bandwidth_tx"
-    lkup[++j] = "msr/irperf"
-    lkup[++j] = "ret_uops_cycles";
-    lkup[++j] = "instructions";
-    lkup[++j] = "uops_retired.retire_slots";
-    lkup[++j] = "cpu_clk_unhalted.thread_any"
-    lkup[++j] = "idq_uops_not_delivered.core"
-    lkup[++j] = "offcore_requests.demand_data_rd"
-    lkup[++j] = "offcore_requests_outstanding.demand_data_rd"
-    lkup[++j] = "qpi_data_bandwidth_tx"
-    lkup[++j] = "unc_c_tor_inserts.miss_opcode.0x182"
-    lkup[++j] = "unc_c_tor_occupancy.miss_opcode.0x182"
-    lkup[++j] = "unc_c_clockticks"
+    lkup[++j] = "qpi_data_bandwidth_tx0";  qpi0 = j;
+    lkup[++j] = "qpi_data_bandwidth_tx1";  qpi1 = j;
+    lkup[++j] = "qpi_data_bandwidth_tx2";  qpi2 = j;
+    lkup[++j] = "unc_cha_tor_inserts.ia_miss.0x40433";   unc_cha_miss = j;
+    lkup[++j] = "unc_cha_tor_occupancy.ia_miss.0x40433"; unc_cha_occ = j;
+    lkup[++j] = "unc_cha_clockticks";  unc_cha_clk = j;
+    lkup[++j] = "power/energy-pkg/";   pwr = j;
+    lkup[++j] = "ref-cycles";          ref_cycles = j;
+    lkup[++j] = "cycles";              cyc = j;
+    lkup[++j] = "offcore_requests.l3_miss_demand_data_rd";              offc_dmnd_data_rd = j;
+    lkup[++j] = "offcore_requests_outstanding.l3_miss_demand_data_rd";  offc_out_dmnd_data_rd = j;
+    lkup[++j] = "unc_cha_tor_inserts.ia.0x40433";        unc_cha_ref = j;
+    lkup[++j] = "upi_data_bandwidth_tx";                 unc_upi_bytes = j;
+    lkup[++j] = "msr/aperf";                             aperf     = j;
+    lkup[++j] = "msr/mperf";                             mperf     = j;
+    lkup[++j] = "msr/irperf";                            irperf    = j;
+    lkup[++j] = "ret_uops_cycles";                       ret_cycles = j;
+    lkup[++j] = "instructions";                          instr     = j;
+    lkup[++j] = "uops_retired.retire_slots";             ret_slots = j;
+    lkup[++j] = "cpu_clk_unhalted.thread_any";           thr_any   = j;
+    lkup[++j] = "idq_uops_not_delivered.core";           not_deliv = j;
+    #lkup[++j] = "offcore_requests.demand_data_rd"
+    #lkup[++j] = "offcore_requests_outstanding.demand_data_rd"; 
+    lkup[++j] = "qpi_data_bandwidth_tx";                 qpi_tx  = j;
+    lkup[++j] = "unc_c_tor_inserts.miss_opcode.0x182";   tor_ins = j;
+    lkup[++j] = "unc_c_tor_occupancy.miss_opcode.0x182"; tor_occ = j;
+    lkup[++j] = "unc_c_clockticks";                      tor_clk = j;
+    lkup[++j] = "L3_lat_out_misses";    L3m = j;
+    lkup[++j] = "L3_accesses";          L3a = j;
+    lkup[++j] = "hwprefetch_local";     pfl = j;
+    lkup[++j] = "hwprefetch_remote";    pfr = j;
+    lkup[++j] = "mem_local";            meml = j;
+    lkup[++j] = "mem_remote";           memr = j;
+    lkup[++j] = "unc_read_write";       unc_rdwr = j;
+    lkup[++j] = "topdown-retiring";     icx_topd_ret = j;
+    lkup[++j] = "topdown-bad-spec";     icx_topd_bs = j;
+    lkup[++j] = "topdown-be-bound";     icx_topd_be = j;
+    lkup[++j] = "cpu/slots/";           icx_topd_slots = j;
+    lkup[++j] = "int_misc.uop_dropping"; icx_uop_drop = j;
+    lkup[++j] = "int_misc.recovery_cycles"; icx_recovery_cycles = j;
 
     lkup_mx = j;
 
@@ -88,35 +105,44 @@ awk -v sockets="${LSCPU_INFO[3]}" -v vendor="${LSCPU_INFO[2]}" -v tsc_ghz="${LSC
       if (!(v in evt_list)) {
         #if (j == 3) { k = L3m; }
         #else if (j == 4) { k = L3cyc; }
-        if (j == 7) { k = mpf; }
-        else if (j == 8) { k = L3m; }
-        else if (j == 9) { k = L3cyc; }
-        else { k = ++i; }
-        if (j == 1) { qpi0 = k; }
-        if (j == 2) { qpi1 = k; }
-        if (j == 3) { unc_cha_miss = k; }
-        if (j == 4) { unc_cha_occ  = k; }
-        if (j == 5) { unc_cha_clk  = k; }
-        if (j == 6) { pwr  = k; }
-        if (j == 7) { offc_pwr  = k; }
-        if (j == 8) { offc_dmnd_data_rd    = k; }
-        if (j == 9) { offc_out_dmnd_data_rd= k; }
-        if (j == 10) { unc_cha_ref  = k; }
-        if (j == 11) { unc_upi_bytes= k; }
-        if (j == 12) { instr        = k; }
-        if (j == 13) { ret_cycles   = k; }
-        if (j == 14) { instr        = k; }
-        if (j == 15) { ret_slots    = k; }
-        if (j == 16) { thr_any      = k; }
-        if (j == 17) { not_deliv    = k; }
-        #if (j == 18) { offc_dmnd_data_rd    = k; }
-        #if (j == 19) { offc_out_dmnd_data_rd= k; }
-        if (j == 20) { qpi_tx = k;}
-        if (j == 21) { tor_ins = k;}
-        if (j == 22) { tor_occ = k;}
-        if (j == 23) { tor_clk = k;}
-        evt_list[v] = k;
-        evt_lkup[i] = v;
+        #if (j == 7) { k = mpf; }
+        #else if (j == 8) { k = L3m; }
+        #else if (j == 9) { k = L3cyc; }
+        #else { k = ++i; }
+        k = j;
+#        ck=0;
+#        if (j == ++ck) { qpi0 = k; }
+#        if (j == ++ck) { qpi1 = k; }
+#        if (j == ++ck) { qpi2 = k; }
+#        if (j == ++ck) { unc_cha_miss = k; }
+#        if (j == ++ck) { unc_cha_occ  = k; }
+#        if (j == ++ck) { unc_cha_clk  = k; }
+#        if (j == ++ck) { pwr  = k; }
+#        if (j == ++ck) { offc_pwr  = k; }
+#        if (j == ++ck) { offc_dmnd_data_rd    = k; }
+#        if (j == ++ck) { offc_out_dmnd_data_rd= k; }
+#        if (j == ++ck) { unc_cha_ref  = k; }
+#        if (j == ++ck) { unc_upi_bytes= k; }
+#        if (j == ++ck) { instr        = k; }
+#        if (j == ++ck) { ret_cycles   = k; }
+#        if (j == ++ck) { cyc          = k; }
+#        if (j == ++ck) { instr        = k; }
+#        if (j == ++ck) { ret_slots    = k; }
+#        if (j == ++ck) { thr_any      = k; }
+#        if (j == ++ck) { not_deliv    = k; }
+#        ck += 2;
+#        #if (j == 18) { offc_dmnd_data_rd    = k; }
+#        #if (j == 19) { offc_out_dmnd_data_rd= k; }
+#        if (j == ++ck) { qpi_tx = k;}
+#        if (j == ++ck) { tor_ins = k;}
+#        if (j == ++ck) { tor_occ = k;}
+#        if (j == ++ck) { tor_clk = k;}
+#        if (ck != lkup_mx) {
+#          printf("messup here, ck= %d, lkup_mx= %d. bye\n", ck, lkup_mx) > "/dev/stderr";
+#          exit(1);
+#        }
+        evt_list[v] = j;
+        evt_lkup[j] = v;
       }
     }
     evt_last = i;
@@ -129,9 +155,10 @@ awk -v sockets="${LSCPU_INFO[3]}" -v vendor="${LSCPU_INFO[2]}" -v tsc_ghz="${LSC
   }
   {
     j = 0;
+#   some events are alternative ways to get the same count (like msr/aperf/ is same as cycles (but msr/aperf/ doesnt use up an event counter)
     if (index($0, "msr/aperf/") > 0) { j=cyc; }
     else if (index($0, "L3_lat_out_cycles") > 0) { j=L3cyc; }
-    else if (index($0, "msr/mperf/") > 0) { j=mpf; }
+    else if (index($0, "msr/mperf/") > 0) { j=mperf; }
     else if (index($0, "msr/irperf/") > 0) { j=instr; }
     else if (index($0, "instructions") > 0) { j=instr; }
     else if (index($0, "ret_uops_cycles") > 0) { j=ret_cycles; }
@@ -154,53 +181,11 @@ awk -v sockets="${LSCPU_INFO[3]}" -v vendor="${LSCPU_INFO[2]}" -v tsc_ghz="${LSC
       next;
     }
   }
-  /L3_lat_out_misses/{
-   n=split($1, arr, ";");
-   tm_i = ck_tm(arr[1]);
-   evt[L3m,tm_i] += arr[2];
-      next;
-  }
-  /L3_accesses/{
-   n=split($1, arr, ";");
-   tm_i = ck_tm(arr[1]);
-   evt[L3a,tm_i] += arr[2];
-      next;
-  }
-  /energy-pkg/{
-   n=split($1, arr, ";");
-   tm_i = ck_tm(arr[1]);
-   evt[pwr,tm_i] += arr[2];
-      next;
-  }
-  /hwprefetch_local/{
-   n=split($1, arr, ";");
-   tm_i = ck_tm(arr[1]);
-   evt[pfl,tm_i] += arr[2];
-      next;
-  }
-  /hwprefetch_remote/{
-   n=split($1, arr, ";");
-   tm_i = ck_tm(arr[1]);
-   evt[pfr,tm_i] += arr[2];
-      next;
-  }
-  /mem_local/{
-   n=split($1, arr, ";");
-   tm_i = ck_tm(arr[1]);
-   evt[meml,tm_i] += arr[2];
-      next;
-  }
-  /mem_remote/{
-   n=split($1, arr, ";");
-   tm_i = ck_tm(arr[1]);
-   evt[memr,tm_i] += arr[2];
-      next;
-  }
   /unc._read_write/{
    n=split($1, arr, ";");
    tm_i = ck_tm(arr[1]);
-   evt[UNC,tm_i] += arr[2]+0;
-   #printf("unc_rd_wr %s, v= %s bw= %f\n", arr[4], arr[2], 64.0e-9*evt[UNC,tm_i]) > "/dev/stderr";
+   evt[unc_rdwr,tm_i] += arr[2]+0;
+   #printf("unc_rd_wr %s, v= %s bw= %f\n", arr[4], arr[2], 64.0e-9*evt[unc_rdwr,tm_i]) > "/dev/stderr";
    #exit;
       next;
  }
@@ -208,14 +193,14 @@ awk -v sockets="${LSCPU_INFO[3]}" -v vendor="${LSCPU_INFO[2]}" -v tsc_ghz="${LSC
    #exit;
    tm_lkup[0] = 0.0;
    cats=0;
-   if (evt[UNC,1] != "") {
+   if (evt[unc_rdwr,1] != "") {
       h[++cats] = "mem_bw";
    }
    if (evt[L3m,1] != "" && evt[L3a,1] != "") {
       h[++cats] = "%L3_miss";
    }
    got_freq=0;
-   if (evt[mpf,1] != "" && evt[cyc,1] != "" && tsc_ghz != "" && num_cpus != "") {
+   if (evt[mperf,1] != "" && evt[cyc,1] != "" && tsc_ghz != "" && num_cpus != "") {
       h[++cats] = "%busy";
       h[++cats] = "frqGHz";
       got_freq=1;
@@ -238,8 +223,28 @@ awk -v sockets="${LSCPU_INFO[3]}" -v vendor="${LSCPU_INFO[2]}" -v tsc_ghz="${LSC
    if (evt[ret_slots,1] != "" && evt[thr_any,1] != "") {
       h[++cats] = "%retiring";
       td_ret = cats;
+   } 
+   if (evt[icx_topd_slots,1] != "" && evt[icx_topd_ret,1] != "") {
+      h[++cats] = "%td_ret";
+      icx_td_ret = cats;
+      got_icx_td_ret = 1;
+   } 
+   if (evt[icx_topd_slots,1] != "" && evt[icx_topd_bs,1] != "") {
+      h[++cats] = "%td_bs";
+      icx_td_bs = cats;
+      got_icx_td_bs = 1;
+   } 
+   if (evt[icx_topd_slots,1] != "" && evt[icx_topd_be,1] != "") {
+      h[++cats] = "%td_be";
+      icx_td_be = cats;
+      got_icx_td_be = 1;
+   } 
+   if (evt[icx_topd_slots,1] != "" && evt[not_deliv,1] != "" && evt[icx_uop_drop,1] != "") {
+      h[++cats] = "%td_fe";
+      icx_td_fe = cats;
+      got_icx_td_fe = 1;
    }
-   if (evt[not_deliv,1] != "") {
+   if (evt[not_deliv,1] != "" && evt[icx_topd_slots,1] == "") {  # so not icx
       h[++cats] = "%frt_end";
       td_frnt = cats;
    }
@@ -250,7 +255,7 @@ awk -v sockets="${LSCPU_INFO[3]}" -v vendor="${LSCPU_INFO[2]}" -v tsc_ghz="${LSC
    if (evt[ret_cycles,1] != "" && evt[cyc,1] != "") {
       h[++cats] = "%ret_cyc";
    }
-   if (evt[pwr,1] != "" && evt[pwr,1] != "") {
+   if (evt[pwr,1] != "") {
       h[++cats] = "pkg_watts";
    }
    if (evt[unc_cha_miss,1] != "" && evt[unc_cha_occ,1] != "") {
@@ -291,11 +296,11 @@ awk -v sockets="${LSCPU_INFO[3]}" -v vendor="${LSCPU_INFO[2]}" -v tsc_ghz="${LSC
     tm_dff = tm_lkup[i] - tm_lkup[i-1];
     tm_elap = tm_lkup[i];
     printf("%8.3f", tm_elap);
-    #printf(" UNC=%d, evt[UNC,%d] %f\n", UNC,i,evt[UNC,i]);
+    #printf(" unc_rdwr=%d, evt[unc_rdwr,%d] %f\n", unc_rdwr,i,evt[unc_rdwr,i]);
     LatUncNs = 0.0;
     for (j=1; j <= cats; j++) {
       v = 0.0;
-      if (h[j] == "mem_bw") { v = 64.0e-9 * evt[UNC,i]/tm_dff; }
+      if (h[j] == "mem_bw") { v = 64.0e-9 * evt[unc_rdwr,i]/tm_dff; }
       if (h[j] == "%L3_miss") {
         if (index(vendor, "AMD") > 0) {
           if(evt[L3a,i] > 0.0) {
@@ -335,7 +340,6 @@ awk -v sockets="${LSCPU_INFO[3]}" -v vendor="${LSCPU_INFO[2]}" -v tsc_ghz="${LSC
       }
       if (h[j] == "LatUnc(ns)") {
           #v = evt[unc_cha_occ,i]/evt[unc_cha_miss,i]/(sockets*evt[unc_cha_occ,i,"inst"]);
-#"expression" : "(1000000000 * [UNC_CHA_TOR_OCCUPANCY.IA_MISS.0x40433] / [UNC_CHA_TOR_INSERTS.IA_MISS.0x40433]) / ( [UNC_CHA_CLOCKTICKS] / ([const_cha_count] * [const_socket_count]) )"
           v  = 0.0;
           v1 = 0.0;
           v2 = 0.0;
@@ -344,7 +348,6 @@ awk -v sockets="${LSCPU_INFO[3]}" -v vendor="${LSCPU_INFO[2]}" -v tsc_ghz="${LSC
           }
           
           if (sockets > 0) { skt = sockets; } else { skt = 1.0; }
-#v1 = (1.0e-9*evt[unc_cha_clk,i]/tm_dff)/(skt*evt[unc_cha_clk,i,"inst"]);
           if (evt[unc_cha_clk,i,"inst"] > 0) {
             v1 = evt[unc_cha_clk,i]/(evt[unc_cha_clk,i,"ns"]);
           }
@@ -366,9 +369,9 @@ awk -v sockets="${LSCPU_INFO[3]}" -v vendor="${LSCPU_INFO[2]}" -v tsc_ghz="${LSC
       if (h[j] == "LatUncCycls") {
           #v = evt[unc_cha_occ,i]/evt[unc_cha_miss,i]/(sockets*evt[unc_cha_occ,i,"inst"]);
 	  if (evt[unc_cha_miss,i] > 0) {
-          v = evt[unc_cha_occ,i]/evt[unc_cha_miss,i];
-          if (sockets > 0) { v /= sockets; }
-          #if (did_sockets_msg != 1) { did_sockets_msg = 1; printf("got sockets= %d\n", sockets) > "/dev/stderr";}
+           v = evt[unc_cha_occ,i]/evt[unc_cha_miss,i];
+           if (sockets > 0) { v /= sockets; }
+           #if (did_sockets_msg != 1) { did_sockets_msg = 1; printf("got sockets= %d\n", sockets) > "/dev/stderr";}
 	  }
           #v = evt[L3cyc,i]/evt[L3m,i];
           if (LatUncNs > 0.0) {
@@ -402,21 +405,45 @@ awk -v sockets="${LSCPU_INFO[3]}" -v vendor="${LSCPU_INFO[2]}" -v tsc_ghz="${LSC
       if (h[j] == "%frt_end") { v = 100.0*evt[not_deliv,i]/(4*evt[cyc,i]/2); td_frt_end_val = v; }
       if (h[j] == "%be_spec") { v = 100 - td_ret_val - td_frt_end_val; if (v < 0) { v = 0.0; }}
       if (h[j] == "%ret_cyc") { v = 100.0*evt[ret_cycles,i]/evt[cyc,i]; }
+      if (h[j] == "%td_ret" || h[j] == "%td_be" || h[j] == "%td_bs" || h[j] == "%td_fe") {
+         if (got_icx_td_sum != i) {
+           got_icx_td_sum  = i;
+           v_td_fctr = 1.0;
+           v_td_ret = 100.0*evt[icx_topd_ret,i]/evt[icx_topd_slots,i];
+           v_td_bs  = 100.0*evt[icx_topd_bs,i]/evt[icx_topd_slots,i];
+           v_td_be  = 100.0*evt[icx_topd_be,i]/evt[icx_topd_slots,i];
+           v_td_fe  = 100.0*(evt[not_deliv,i]-evt[icx_uop_drop,i])/evt[icx_topd_slots,i];
+           if (got_icx_td_ret == 1 && got_icx_td_bs == 1 && got_icx_td_be == 1 && got_icx_td_fe == 1) {
+              v_td_sum = v_td_ret + v_td_bs + v_td_be + v_td_fe;
+              if (v_td_sum != 0.0) { v_td_fctr = 100.0/v_td_sum; }
+              #printf("\nv_td_sum= %f v_td_fctr= %f i= %d fe= %f fctr*fe= %f\n", v_td_sum, v_td_fctr, i, v_td_fe, v_td_fctr * v_td_fe);
+              #v_td_fctr = 1.0;
+           }
+         }
+      }
+      if (h[j] == "%td_ret") { v = v_td_fctr * v_td_ret; }
+      if (h[j] == "%td_bs")  { v = v_td_fctr * v_td_bs; }
+      if (h[j] == "%td_be")  { v = v_td_fctr * v_td_be; }
+      if (h[j] == "%td_fe")  { v = v_td_fctr * v_td_fe; }
+      #if (h[j] == "%td_fe")  { v = v_td_sum ; }
+
       if (h[j] == "bw_rmt") {
         if (evt[qpi0,1] != "" && evt[qpi1,1] != "") {
-          v = (64.0/9.0) * 1.0e-9 * (evt[qpi0,i]+evt[qpi1,i])/tm_dff;
+          v = (64.0/9.0) * 1.0e-9 * (evt[qpi0,i]+evt[qpi1,i]+evt[qpi2,i])/tm_dff;
         }
         if (evt[unc_upi_bytes,1] != "") {
           v = 1.0e-9 * (evt[unc_upi_bytes,i])/tm_dff;
         }
       }
       if (h[j] == "pf_lcl") { v = 64.0e-9 * evt[pfl,i]/tm_dff; }
-      if (h[j] == "pkg_watts") { v = evt[pwr,i]/tm_dff; }
+      if (h[j] == "pkg_watts") { v = evt[pwr,i]/tm_dff; 
+        #printf("\npwr: evt[%d,%d]= %f tm_diff= %f v= %f\n", pwr,i, evt[pwr,i], tm_dff, v);
+      }
       if (h[j] == "pf_rmt") { v = 64.0e-9 * evt[pfr,i]/tm_dff; }
       if (h[j] == "dmnd_lcl") { v = 64.0e-9 * evt[meml,i]/tm_dff; }
       if (h[j] == "dmnd_rmt") { v = 64.0e-9 * evt[memr,i]/tm_dff; }
-      if (h[j] == "%busy") { bsy = 1.0e-9*(evt[mpf,i])/(num_cpus * tsc_ghz * tm_dff); v = 100.0*bsy;}
-      if (h[j] == "frqGHz") { frqGHz = tsc_ghz * evt[cyc,i]/evt[mpf,i]; v = frqGHz;}
+      if (h[j] == "%busy") { bsy = 1.0e-9*(evt[mperf,i])/(num_cpus * tsc_ghz * tm_dff); v = 100.0*bsy;}
+      if (h[j] == "frqGHz") { frqGHz = tsc_ghz * evt[cyc,i]/evt[mperf,i]; v = frqGHz;}
       printf("%s%8.3f", dlm, v);
     }
     printf("\n");
