8f679523090001810314aabab5fa5f0ac73f0a75
Patrick Fay <pfay1@ext.uber.com>
2021-08-30 22:44:31 -0400

    change locale to C.UTF-8

--- decode_cpu_fam_mod.sh
+++ decode_cpu_fam_mod.sh
@@ -5,10 +5,10 @@ INF=/proc/cpuinfo
 if [ "$1" != "" ]; then
   INF=$1
 fi
-export LANGUAGE=en_US.UTF-8
-export LC_ALL=en_US.UTF-8
-export LANG=en_US.UTF-8
-export LC_CTYPE=en_US.UTF-8
+export LANGUAGE=C.UTF-8
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
+export LC_CTYPE=C.UTF-8
 export LC_ALL=C
 
 
